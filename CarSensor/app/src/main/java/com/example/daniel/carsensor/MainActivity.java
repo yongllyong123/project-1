80716bede6bd1cdac3cb49d892923036b7317210
dlynch7 <daniellynch2021@u.northwestern.edu>
2018-03-04 23:17:48 -0600

    using JSoup to scrape content off webpage

--- CarSensor/app/src/main/java/com/example/daniel/carsensor/MainActivity.java
+++ CarSensor/app/src/main/java/com/example/daniel/carsensor/MainActivity.java
@@ -3,11 +3,65 @@ package com.example.daniel.carsensor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button getBtn;
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        result = (TextView) findViewById(R.id.result);
+        getBtn = (Button) findViewById(R.id.getBtn);
+        getBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getWebsite();
+            }
+        });
+    }
+
+    private void getWebsite() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                final StringBuilder builder = new StringBuilder();
+
+                try {
+//                    Document doc = Jsoup.connect("http://www.ssaurel.com/blog").get();
+                    Document doc = Jsoup.connect("http://10.105.176.164:8080").get();
+                    String title = doc.title();
+                    Elements links = doc.select("body");
+
+                    builder.append(title).append("\n");
+
+                    for (Element link : links) {
+                        builder.append("\n").append("Link : ").append(link.attr("href"))
+                                .append("\n").append("Text : ").append(link.text());
+                    }
+                } catch (IOException e) {
+                    builder.append("Error : ").append(e.getMessage()).append("\n");
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        result.setText(builder.toString());
+                    }
+                });
+            }
+        }).start();
     }
 }
